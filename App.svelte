b430efed23ff7e466cf804da50fd79767d4c21a8
Tyler McRobert <31973492+tylermcrobert@users.noreply.github.com>
2020-12-23 20:34:33 -0500

    Initial commit

---
+++ App.svelte
@@ -0,0 +1,16 @@
+<script>
+	import Video from "./Video.svelte";
+</script>
+
+<style>
+  main {
+    font-family: sans-serif;
+    text-align: center;
+  }
+</style>
+
+<main>
+	<h1>Hello CodeSandbox</h1>
+	<h2>Start editing to see some magic happen!</h2>
+	<Video />
+</main>
---
+++ Video.svelte
@@ -0,0 +1,16 @@
+<script>
+  let url =
+    "http://phpstack-177854-1073625.cloudwaysapps.com/videos/Ak_Rishiri_loop.mp4";
+</script>
+
+<style>
+	video {
+	  width: 100%;
+	  display: block;
+	}
+</style>
+
+
+<div>
+	<video src={url}/>
+</div>
